14913695d11edd28b5bcf070b42597705f4e45ea
Ryan Rafael <ryrafael461@gmail.com>
2023-07-28 17:42:00 -0300

    possibilidades de movimentação das peças

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	protected Position position;
 	private Board board;
 	
@@ -12,5 +12,21 @@ public class Piece {
 		return board;
 	}
 	
+	public abstract boolean[][] possibleMoves();
 	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for(int x=0; x<mat.length; x++) {
+			for(int y=0; y<mat.length; y++) {
+				if(mat[x][y]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
